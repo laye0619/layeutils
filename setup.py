88396ccd878739a04a157eac56785c05f988badc
laye0619 <laye0619@gmail.com>
2023-12-18 14:02:32 +0800

    new

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-
 setuptools.setup(
     name="layeutils",
     version="0.1.4",
     author="LayeWang",
     author_email="laye0619@gmail.com",
     description="Private Utils",
-    long_description="",
+    long_description="Laye private utils",
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
