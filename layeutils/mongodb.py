6eabba89c5c778139abc50e3547e96463169bd04
laye0619 <laye0619@gmail.com>
2024-02-25 20:54:47 +0800

    bug fix

--- layeutils/mongodb.py
+++ layeutils/mongodb.py
@@ -4,12 +4,12 @@ from loguru import logger
 
 
 def db_save_dict_to_mongodb(
-        mongo_db_client_linke: str,
+        mongo_db_client_link: str,
         mongo_db_name: str,
         col_name: str,
         target_dict: dict
 ):
-    c = pymongo.MongoClient(mongo_db_client_linke)
+    c = pymongo.MongoClient(mongo_db_client_link)
     db = c[mongo_db_name]
     db_col = db[col_name]
     if not isinstance(target_dict, list):
@@ -22,14 +22,14 @@ def db_save_dict_to_mongodb(
 
 
 def db_update_dict_to_mongodb(
-        mongo_db_client_linke: str,
+        mongo_db_client_link: str,
         mongo_db_name: str,
         col_name: str,
         query_dict: dict,
         target_dict: dict,
         upsert: bool = False
 ):
-    c = pymongo.MongoClient(mongo_db_client_linke)
+    c = pymongo.MongoClient(mongo_db_client_link)
     db = c[mongo_db_name]
     db_col = db[col_name]
     update_result = db_col.update_one(
@@ -38,7 +38,7 @@ def db_update_dict_to_mongodb(
 
 
 def db_get_dict_from_mongodb(
-        mongo_db_client_linke: str,
+        mongo_db_client_link: str,
         mongo_db_name: str,
         col_name: str,
         query_dict: dict = {},
@@ -54,7 +54,7 @@ def db_get_dict_from_mongodb(
     :return:
     '''
     c = pymongo.MongoClient(
-        host=mongo_db_client_linke,
+        host=mongo_db_client_link,
         tz_aware=True,
         tzinfo=pytz.timezone('Asia/Chongqing')
     )
@@ -67,14 +67,14 @@ def db_get_dict_from_mongodb(
 
 
 def db_get_distinct_from_mongodb(
-        mongo_db_client_linke: str,
+        mongo_db_client_link: str,
         mongo_db_name: str,
         col_name: str,
         field: str,
         query_dict: dict = {}
 ):
     c = pymongo.MongoClient(
-        host=mongo_db_client_linke,
+        host=mongo_db_client_link,
         tz_aware=True,
         tzinfo=pytz.timezone('Asia/Chongqing')
     )
@@ -85,12 +85,12 @@ def db_get_distinct_from_mongodb(
 
 
 def db_del_dict_from_mongodb(
-        mongo_db_client_linke: str,
+        mongo_db_client_link: str,
         mongo_db_name: str,
         col_name: str,
         query_dict: dict
 ):
-    c = pymongo.MongoClient(mongo_db_client_linke)
+    c = pymongo.MongoClient(mongo_db_client_link)
     db = c[mongo_db_name]
     db_col = db[col_name]
     x = db_col.delete_many(query_dict)
