6cf16c00ba246fe1d470fff72a760ba06d05b96a
laye0619 <laye0619@gmail.com>
2024-03-29 13:29:52 +0800

    add image process

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="layeutils",
-    version="0.2.9",
+    version="0.2.10",
     author="LayeWang",
     author_email="laye0619@gmail.com",
     description="Private Utils",
