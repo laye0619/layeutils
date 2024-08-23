2845db9ea9f607a29e3b4b07c7ae2093e0a60c8c
laye0619 <laye0619@gmail.com>
2024-08-23 20:49:13 +0800

    update

--- layeutils/telegram.py
+++ layeutils/telegram.py
@@ -1,10 +1,10 @@
 
-def send_message(bot_token: str, chat_id: str, msg: str):
+def send_message(bot_token: str, msg: str, chat_id: str = '6097092665'):
     """Simple sending telegram message via pyTelegramBotAPI
 
     Args:
         bot_token (str): _description_
-        chat_id (str): _description_
+        chat_id (str): omit this param will send msg to myself
         msg (str): _description_
 
     Returns:
@@ -13,4 +13,8 @@ def send_message(bot_token: str, chat_id: str, msg: str):
     import telebot
 
     bot = telebot.TeleBot(bot_token)
-    return bot.send_message(chat_id, msg)
+    return bot.send_message(
+        chat_id=chat_id, 
+        text=msg, 
+        parse_mode='MarkdownV2'
+    )
