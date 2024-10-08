14d5712763b62cf982a7a158649792098dda07d0
laye0619 <laye0619@gmail.com>
2024-10-08 08:44:23 +0800

    update homeproxy port to 7890

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="layeutils",
-    version="0.2.32",
+    version="0.2.33",
     author="LayeWang",
     author_email="laye0619@gmail.com",
     description="Private Utils",
