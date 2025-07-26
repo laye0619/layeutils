7aa7661fe105d90920b1d0a579819ee4c0ebbb84
laye0619 <laye0619@gmail.com>
2025-07-26 10:33:58 +0100

    bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="layeutils",
-    version="0.2.39,
+    version="0.2.39",
     author="LayeWang",
     author_email="laye0619@gmail.com",
     description="Private Utils",
