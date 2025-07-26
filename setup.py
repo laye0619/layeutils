efb91ad042132b17bbd1f925358cdcdd92788f28
laye0619 <laye0619@gmail.com>
2025-07-26 10:30:32 +0100

    add m3u process

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="layeutils",
-    version="0.2.37",
+    version="0.2.38",
     author="LayeWang",
     author_email="laye0619@gmail.com",
     description="Private Utils",
