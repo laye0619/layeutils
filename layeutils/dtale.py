1339775c40e53cde40623ac48632183e0fdbbcc0
laye0619 <laye0619@gmail.com>
2025-08-19 22:25:00 +0100

    修复dtale

--- layeutils/dtale.py
+++ layeutils/dtale.py
@@ -1,19 +1,15 @@
 import pandas as pd
 
 
-def dtale_start(df: pd.DataFrame, precision=None) -> str:
-    """利用Vscode端口映射启动dtale
+def dtale_start(df: pd.DataFrame, **options):
+    """利用Vscode端口映射启动dtale，强制指定host为0.0.0.0
 
     Args:
         df (pd.DataFrame): 目标DF
+        options: 其他参数
 
     Returns:
-        str: dtale内部地址
+        dtale.show
     """
     import dtale
-    d = dtale.show(df, precision=precision)
-    # return f"http://localhost:{d._main_url.split(':')[-1]}"
-    return [
-        f"https://dtale.home.laye.wang:18443/dtale{d._main_url.split('dtale')[-1]}",
-        f"http://192.168.1.85:{d._main_url.split(':')[-1]}"
-    ]
+    return dtale.show(df, host='0.0.0.0', **options)
